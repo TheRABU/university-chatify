d330058232a92980a6283f1ea89b95b865d69488
TheRABU <mdfazlerabbi114@gmail.com>
2024-03-16 02:16:43 +0600

    added a lots of things in the backend using jwttokens,userControllers,userModel,userRoutes etc and till now it works!

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-const app = express();
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db");
+
 dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json()); // bhai please except the json data
+
 // express api
 app.get("/", (request, response) => {
   response.send("API is running successfully ");
 });
+
+// app.use("/api/user");
+
 // chat object show
-app.get("/api/chat", (request, response) => {
-  response.send(chats);
-});
+// app.get("/api/chat", (request, response) => {
+//   response.send(chats);
+// });
+
 app.get("/api/chat/:id", (request, response) => {
   console.log(request.params.id);
   // find a single chat
   const singleChat = chats.find((c) => c._id === request.params.id);
   response.send(singleChat);
 });
+
 // env port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
